71507a2ca826f884d764c0f71c62ce67f9d317ec
Vdepaa <devasaputra.kc@gmail.com>
2026-05-22 14:21:22 +0800

    Create customer migration, model, controller, and routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\CustomerController;
 
 Route::apiResource('services', ServiceController::class);
 Route::patch('services/{service}/activate', [ServiceController::class, 'activate']);
 Route::patch('services/{service}/deactivate', [ServiceController::class, 'deactivate']);
+
+Route::apiResource('customers', CustomerController::class);
+Route::patch('customers/{customer}/activate', [CustomerController::class, 'activate']);
+Route::patch('customers/{customer}/deactivate', [CustomerController::class, 'deactivate']);
